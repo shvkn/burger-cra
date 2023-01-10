044c539fcb3bca0c2a55560c173d996841b5cf95
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-11 01:27:40 +0700

    refactor: Выделить селекторы ingredientSlice в отдельный файл

---
+++ src/services/selectors/ingredients.js
@@ -0,0 +1,39 @@
+import { ingredientsAdapter } from '../actions/ingredients';
+import { createSelector } from '@reduxjs/toolkit';
+
+const ingredientsSlice = (state) => state.ingredients;
+
+const {
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal,
+  selectById: _selectById,
+} = ingredientsAdapter.getSelectors(ingredientsSlice);
+
+const selectById = (id) => (state) => _selectById(state, id);
+
+const isSucceeded = createSelector(
+  ingredientsSlice,
+  (ingredients) => ingredients.status === 'succeeded'
+);
+
+const isFailed = createSelector(ingredientsSlice, (ingredients) => ingredients.status === 'failed');
+
+const isLoading = createSelector(
+  ingredientsSlice,
+  (ingredients) => ingredients.status === 'loading'
+);
+
+const ingredientsSelectors = {
+  selectIds,
+  selectEntities,
+  selectAll,
+  selectTotal,
+  selectById,
+  isSucceeded,
+  isFailed,
+  isLoading,
+};
+
+export default ingredientsSelectors;
