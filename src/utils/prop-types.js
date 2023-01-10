e4305a867fc4a8756f3bc0dfe75c2be2eaaa2f4a
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-10 23:45:41 +0700

    feat: Описать PropTypes для объекта заказа из API

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -12,5 +12,13 @@ export const ingredientPropTypes = PropTypes.shape({
   image: PropTypes.string.isRequired,
   image_mobile: PropTypes.string.isRequired,
   image_large: PropTypes.string.isRequired,
-  __v: PropTypes.number.isRequired,
+});
+
+export const orderPropTypes = PropTypes.shape({
+  _id: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  number: PropTypes.number.isRequired,
+  createdAt: PropTypes.string.isRequired,
+  updatedAt: PropTypes.string.isRequired,
+  ingredients: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
 });
