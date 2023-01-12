9ef841103873b96e34def603d7f41f5529218b66
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-12 13:28:26 +0700

    refactor: Изменить ingredientPropTypes.type, orderPropTypes.status в соответствии со значениями из API

--- src/utils/prop-types.js
+++ src/utils/prop-types.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 export const ingredientPropTypes = PropTypes.shape({
   _id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['bun', 'sauce', 'main']).isRequired,
   proteins: PropTypes.number.isRequired,
   fat: PropTypes.number.isRequired,
   carbohydrates: PropTypes.number.isRequired,
@@ -16,7 +16,7 @@ export const ingredientPropTypes = PropTypes.shape({
 
 export const orderPropTypes = PropTypes.shape({
   _id: PropTypes.string.isRequired,
-  status: PropTypes.string.isRequired,
+  status: PropTypes.oneOf(['done', 'created', 'pending']).isRequired,
   number: PropTypes.number.isRequired,
   createdAt: PropTypes.string.isRequired,
   updatedAt: PropTypes.string.isRequired,
