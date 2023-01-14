b9f58cd374ec41cc44389028d0e90e89f5e0aff0
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-15 00:02:53 +0700

    refactor: Оптимизация верстки компонента OrderStatus

--- src/components/order-status/order-status.jsx
+++ src/components/order-status/order-status.jsx
@@ -2,27 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { OrderStatuses } from '../../utils/constants';
 
-function OrderStatus({ status, className = '' }) {
-  let value;
-  switch (status) {
-    case 'done':
-      value = 'Выполнен';
-      break;
-    case 'created':
-      value = 'Создан';
-      break;
-    case 'pending':
-      value = 'В работе';
-      break;
-    default:
-      value = status;
-  }
-  return <div className={className}>{value}</div>;
+const dict = {
+  done: 'Выполнен',
+  created: 'Создан',
+  pending: 'В работе',
+};
+
+function OrderStatus({ status }) {
+  return <span>{dict[status.toLowerCase()]}</span>;
 }
 
 OrderStatus.propTypes = {
   status: PropTypes.oneOf(Object.values(OrderStatuses)).isRequired,
-  className: PropTypes.string,
 };
 
 export default OrderStatus;
