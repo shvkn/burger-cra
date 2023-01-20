df986a4e120de40a45d0edbcf011e8208843b490
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-21 00:43:17 +0700

    refactor: Рефакторинг страницы 404

--- src/pages/not-founded/not-founded.jsx
+++ src/pages/not-founded/not-founded.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import styles from './not-founded.module.css';
+import { Button } from '@ya.praktikum/react-developer-burger-ui-components';
+import { useHistory } from 'react-router-dom';
 
 function NotFoundedPage() {
+  const history = useHistory();
+  const handleClick = () => {
+    history.replace({ pathname: '/' });
+  };
   return (
-    <div>
-      <h1 className={'text text_type_main-large'}>404</h1>
-      <h2 className={'text text_type_main-large'}>Запрашиваемая страница не найдена</h2>
-    </div>
+    <main className={styles.layout}>
+      <h1 className={'text text_type_main-large text_color_error'}>404</h1>
+      <p className={'mt-8 mb-15 text text_type_main-medium'}>Похоже вы потерялись :(</p>
+      <Button htmlType={'button'} onClick={handleClick}>
+        На главную
+      </Button>
+    </main>
   );
 }
 
