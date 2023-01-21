ec38aba8596e4fc1998edd512aef100e7276fd0d
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-01-21 22:41:40 +0700

    refactor: Рефакторинг burgerSlice

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -6,7 +6,7 @@ import PropTypes from 'prop-types';
 function OrderDetails({ number }) {
   return (
     <>
-      <p className={`mt-4 text text_type_digits-large ${styles.id}`}>{number}</p>
+      <p className={`mt-30 text text_type_digits-large ${styles.id}`}>{number}</p>
       <p className={`mt-8 text text_type_main-medium`}>идентификатор заказа</p>
       <img className={`mt-15`} src={doneImage} alt='Заказ оформлен' />
       <p className={`mt-15 text text_type_main-default`}>Ваш заказ начали готовить</p>
--- src/services/slices/burger.js
+++ src/services/slices/burger.js
@@ -19,22 +19,27 @@ const burger = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(addIngredient, (state, { payload: { id, uid } }) => {
+      .addCase(addIngredient, (state, action) => {
+        const { id, uid } = action.payload;
         state.ingredients.push({ id, uid });
         state.counts[id] = state.counts[id] + 1 || 1;
       })
-      .addCase(setBun, (state, { payload: id }) => {
+      .addCase(setBun, (state, action) => {
+        console.log(action);
+        const id = action.payload;
         delete state.counts[state.bun];
         state.bun = id;
         state.counts[id] = 2;
       })
-      .addCase(removeIngredient, (state, { payload: index }) => {
+      .addCase(removeIngredient, (state, action) => {
+        const index = action.payload;
         const { id } = state.ingredients[index];
         state.ingredients.splice(index, 1);
         state.counts[id] = state.counts[id] - 1;
         if (state.counts[id] <= 0) delete state.counts[id];
       })
-      .addCase(moveIngredient, (state, { payload: { hoverIndex, dragIndex } }) => {
+      .addCase(moveIngredient, (state, action) => {
+        const { hoverIndex, dragIndex } = action.payload;
         const [dragElement] = state.ingredients.splice(dragIndex, 1);
         state.ingredients.splice(hoverIndex, 0, dragElement);
       })
