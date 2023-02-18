1a66e606684e3e72ccbe8b52824bc8f5faa56a8c
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-02-18 23:06:21 +0700

    refactor: Перевести на TSX src/components/modals/ingredient-modal/ingredient-modal

--- src/components/modals/ingredient-modal/ingredient-modal.tsx
+++ src/components/modals/ingredient-modal/ingredient-modal.tsx
@@ -2,21 +2,21 @@ import React from 'react';
 import Modal from 'components/modal';
 import IngredientDetails from 'components/ingredient-details';
 import { useHistory, useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
 import ingredientsSelectors from 'services/selectors/ingredients';
+import { useAppSelector } from 'services/slices';
 
-function IngredientModal() {
-  const { id } = useParams();
-  const entities = useSelector(ingredientsSelectors.selectEntities);
-  const ingredient = entities[id];
+const IngredientModal = () => {
+  const { id } = useParams<{ id: string }>();
   const history = useHistory();
+  const ingredient = useAppSelector(ingredientsSelectors.selectById(id));
+
   const handleClose = () => {
     history.goBack();
   };
 
   return ingredient ? (
     <Modal handleClose={handleClose}>
-      <Modal.Header>
+      <Modal.Header handleClose={handleClose}>
         <p className={'text text_type_main-large'}>Детали ингредиента</p>
       </Modal.Header>
       <Modal.Content>
@@ -24,6 +24,6 @@ function IngredientModal() {
       </Modal.Content>
     </Modal>
   ) : null;
-}
+};
 
 export default IngredientModal;
