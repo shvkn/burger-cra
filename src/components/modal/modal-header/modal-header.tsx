2f28cf32fecf433a4123d858eeb4f0d84cea0c76
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-02-20 13:00:38 +0700

    fix: Сделать опциональным пропс children в src/components/modal/modal-header/modal-header

--- src/components/modal/modal-header/modal-header.tsx
+++ src/components/modal/modal-header/modal-header.tsx
@@ -3,14 +3,14 @@ import { CloseIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import React, { FC, ReactNode } from 'react';
 
 export type TModalHeader = {
-  children: ReactNode;
+  children?: ReactNode;
   handleClose: () => void;
 };
 
 const ModalHeader: FC<TModalHeader> = ({ children, handleClose }) => {
   return children ? (
     <div className={`ml-10 mt-10 mr-10 ${styles.container}`}>
-      <div>{children}</div>
+      {children && <div>{children}</div>}
       <button className={styles.close}>
         <CloseIcon type='primary' onClick={handleClose} />
       </button>
