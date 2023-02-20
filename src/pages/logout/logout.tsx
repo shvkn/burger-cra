2f1e5d8acef1d420ebc58597dad367b3b0353a0d
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-02-20 23:08:37 +0700

    refactor: Типизировать pages/logoutˆ

--- src/pages/logout/logout.tsx
+++ src/pages/logout/logout.tsx
@@ -1,16 +1,16 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { FC, useEffect } from 'react';
 import * as authActions from 'services/actions/auth';
 import { Redirect } from 'react-router-dom';
+import { useAppDispatch } from 'services/slices';
 
-function LogoutPage() {
-  const dispatch = useDispatch();
+const LogoutPage: FC = () => {
+  const dispatch = useAppDispatch();
 
   useEffect(() => {
     dispatch(authActions.logout());
   }, [dispatch]);
 
   return <Redirect to={'/'} />;
-}
+};
 
 export default LogoutPage;
