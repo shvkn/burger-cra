b2b4baae8f211bb66e9c05ed3d1e6f1b249b538f
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-03-04 11:29:35 +0700

    refactor(pages/ingredient): Заменить использование services/*/ingredients на entities/ingredient

--- src/pages/ingredient/ingredient.tsx
+++ src/pages/ingredient/ingredient.tsx
@@ -2,14 +2,13 @@ import React, { FC } from 'react';
 import IngredientDetails from 'components/ingredient-details';
 import DetailsLayout from 'components/details-layout';
 import styles from './ingredient.module.css';
-import ingredientsSelectors from 'services/selectors/ingredients';
 import { useParams } from 'react-router-dom';
 import { NotFoundedPage } from 'pages/index';
-import { useAppSelector } from 'services/slices';
+import { ingredientModel } from 'entities/ingredient';
 
 const IngredientPage: FC = () => {
   const { id } = useParams<{ id: TIngredientId }>();
-  const ingredient = useAppSelector(ingredientsSelectors.selectById(id));
+  const ingredient = ingredientModel.useIngredient(id);
   return ingredient ? (
     <main className={`mt-30 ${styles.layout}`}>
       <DetailsLayout>
