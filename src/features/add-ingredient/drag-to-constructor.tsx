1d7aba84f112749be9622288053623d85c0278e1
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-03-04 22:17:32 +0700

    feat(features/add-ingredient/drag-to-constructor): Выделить перетаскивание ингредиента в конструктор в отдельную фичу

---
+++ src/features/add-ingredient/drag-to-constructor.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useDrag } from 'react-dnd';
+import { DndItemTypes } from 'utils/constants';
+
+type TDragToConstructorProps = {
+  ingredient: TIngredient;
+  children: React.ReactNode;
+};
+
+export const DragToConstructor: React.FC<TDragToConstructorProps> = ({ ingredient, children }) => {
+  const [, dragRef] = useDrag({
+    type: DndItemTypes.BURGER_INGREDIENT,
+    item: { id: ingredient._id },
+  });
+  return <div ref={dragRef}>{children}</div>;
+};
