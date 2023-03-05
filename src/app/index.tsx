9ba7b4f52fa8f884eecfc8cb58f050bebb631bd0
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-03-05 16:16:21 +0700

    refactor(components/app): Вынести в слой App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,10 @@
-import React, { FC } from 'react';
+import React from 'react';
 import 'style/common.css';
 import '@ya.praktikum/react-developer-burger-ui-components';
 import { Routes } from 'pages';
 import AppLayout from 'components/app-layout/app-layout';
 
-const App: FC = () => {
+const App: React.FC = () => {
   return (
     <AppLayout>
       <Routes />
