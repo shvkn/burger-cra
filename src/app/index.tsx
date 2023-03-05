d1f4446496020b22fa57df0c8b09b44a2acc0a45
Anatoly Shuvakhin (shvkn) <shuvakhin@gmail.com>
2023-03-05 17:21:49 +0700

    refactor(components/app-layout): Вынести в слой widgets

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import 'style/common.css';
 import '@ya.praktikum/react-developer-burger-ui-components';
 import { Routes } from 'pages';
-import AppLayout from 'components/app-layout/app-layout';
+import { AppLayout } from 'widgets/app-layout';
 import { withProviders } from './providers';
 
 const App = () => {
--- src/widgets/app-layout/app-layout.tsx
+++ src/widgets/app-layout/app-layout.tsx
@@ -1,16 +1,14 @@
 import React, { FC, ReactNode } from 'react';
-import styles from './app-layout.module.css';
+import styles from 'widgets/app-layout/app-layout.module.css';
 import AppHeader from 'widgets/app-header';
 
 type TAppLayoutProps = {
   children: ReactNode;
 };
 
-const AppLayout: FC<TAppLayoutProps> = ({ children }) => (
+export const AppLayout: FC<TAppLayoutProps> = ({ children }) => (
   <div className={styles.layout}>
     <AppHeader />
     <div className={styles.mainContainer}>{children}</div>
   </div>
 );
-
-export default AppLayout;
